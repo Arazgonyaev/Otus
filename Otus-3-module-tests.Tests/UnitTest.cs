b2f1ec3d544a33d770e2019a384d68020899b9dd
Андрей Разгоняев <arazgonaev@MacBook-Air-Andrej.local>
2023-09-16 00:08:42 +0500

    first commit

---
+++ Otus-3-module-tests.Tests/UnitTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Otus_3_module_tests.Tests
+{
+    private double[] coefficients = new double[0];
+
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void NoRoots()
+        {
+            //Arrange
+            coefficients = new double []{ 1, 0, 1};
+        }
+    }
+}
---
+++ Otus-3-module-tests/QuadraticEquationSolver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Otus_3_module_tests
+{
+    public static class QuadraticEquationSolver
+    {
+        public static double[] Solve(double a, double b, double c, double e = 1e-5)
+        {
+            /*if (Math.Abs(a) <= e)
+            {
+                throw new ArgumentException("a равно 0");
+            }
+
+            var D = b * b - 4 * a * c;
+
+            if (D < -e)
+            {
+                return new double[0];
+            }
+            else if (Math.Abs(D) <= e)
+            {
+                return new double[] { -b / (2 * a), -b / (2 * a) };
+            }
+            else // D > e
+            {
+                return new double[] { -b + Math.Sqrt(D)/ (2 * a), (-b - Math.Sqrt(D)) / (2 * a) };
+            }*/
+            return new double[0];
+        }
+    }
+}
