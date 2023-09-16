9645297a11b0f29a5754f4fb91f3496beb3ec473
arazgonaev <arazgonyaev@gmail.com>
2023-09-16 12:34:13 +0500

    Increase framefork to net6

--- Otus-3-module-tests/Program.cs
+++ Otus-3-module-tests/Program.cs
@@ -6,7 +6,7 @@ namespace Otus_3_module_tests
     {
         static void Main(string[] args)
         {
-            var roots = QuadraticEquationSolver.Solve(1, 0, -1);
+            var roots = QuadraticEquationSolver.Solve(1, 2, 1+1e-7);
             Console.WriteLine(roots.Length == 0
                 ? "Корней нет"
                 : $"x1: {roots[0]}, x2: {roots[1]}");
--- Otus-3-module-tests/QuadraticEquationSolver.cs
+++ Otus-3-module-tests/QuadraticEquationSolver.cs
@@ -1,17 +1,25 @@
 ﻿using System;
+using System.Linq;
+
 namespace Otus_3_module_tests
 {
     public static class QuadraticEquationSolver
     {
         public static double[] Solve(double a, double b, double c, double e = 1e-5)
         {
-            /*if (Math.Abs(a) <= e)
+            if (Math.Abs(a) <= e)
             {
-                throw new ArgumentException("a равно 0");
+                throw new ArgumentException("a не должно быть равно 0");
+            }
+
+            var incorrectDoubles = new[] { double.NaN, double.NegativeInfinity, double.PositiveInfinity };
+            if (incorrectDoubles.Intersect(new[] {a, b, c }).Any())
+            {
+                throw new ArgumentException("Коэффициенты должны быть действительными числами");
             }
 
             var D = b * b - 4 * a * c;
-
+            
             if (D < -e)
             {
                 return new double[0];
@@ -23,8 +31,7 @@ namespace Otus_3_module_tests
             else // D > e
             {
                 return new double[] { -b + Math.Sqrt(D)/ (2 * a), (-b - Math.Sqrt(D)) / (2 * a) };
-            }*/
-            return new double[0];
+            }
         }
     }
 }
