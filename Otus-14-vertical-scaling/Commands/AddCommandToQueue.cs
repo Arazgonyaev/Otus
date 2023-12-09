3c4162998d868d7b34f595e9ffe7357d47147abd
arazgonaev <arazgonyaev@gmail.com>
2023-12-09 12:37:32 +0500

    Add exception handling

---
+++ Otus-14-vertical-scaling/Commands/AddCommandToQueue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace Otus_14_vertical_scaling;
+
+public class AddCommandToQueue : ICommand
+{
+    private readonly BlockingCollection<ICommand> queue;
+    private readonly ICommand command;
+
+    public AddCommandToQueue(BlockingCollection<ICommand> queue, ICommand command)
+    {
+        this.queue = queue ?? throw new ArgumentNullException(nameof(queue));
+        this.command = command ?? throw new ArgumentNullException(nameof(command));
+    }
+
+    public void Execute()
+    {
+        queue.Add(command);
+    }
+}
---
+++ Otus-14-vertical-scaling/Exceptions/ILogger.cs
@@ -0,0 +1,6 @@
+﻿namespace Otus_14_vertical_scaling;
+
+public interface ILogger
+{
+    void Log(string message);
+}
